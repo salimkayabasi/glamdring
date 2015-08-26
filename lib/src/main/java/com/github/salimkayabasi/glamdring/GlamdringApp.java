e9ab4136a5aad82d5a11e6d5498dd761caf3d554
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2015-08-26 22:31:41 +0300

    adding butterKnife picasso and App

---
+++ lib/src/main/java/com/github/salimkayabasi/glamdring/GlamdringApp.java
@@ -0,0 +1,32 @@
+package com.github.salimkayabasi.glamdring;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.crashlytics.android.Crashlytics;
+import com.github.salimkayabasi.glamdring.utils.LogUtil;
+import com.github.salimkayabasi.glamdring.utils.PicassoUtil;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import butterknife.ButterKnife;
+import io.fabric.sdk.android.Fabric;
+
+public class GlamdringApp extends Application {
+
+  private static final AtomicReference<GlamdringApp> INSTANCE = new AtomicReference<>();
+
+  public static Context getContext() {
+    return INSTANCE.get();
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    INSTANCE.set(this);
+    Fabric.with(this, new Crashlytics());
+    LogUtil.init();
+    PicassoUtil.init(this);
+    ButterKnife.setDebug(BuildConfig.DEBUG);
+  }
+}
