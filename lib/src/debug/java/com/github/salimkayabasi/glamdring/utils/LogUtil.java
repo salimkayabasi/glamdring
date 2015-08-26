57584f7017b2d38b9c9c16352a3322f6158947b4
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2015-08-26 22:12:41 +0300

    remove stetho dependencies inside of LogUtil

--- lib/src/debug/java/com/github/salimkayabasi/glamdring/utils/LogUtil.java
+++ lib/src/debug/java/com/github/salimkayabasi/glamdring/utils/LogUtil.java
@@ -1,7 +1,5 @@
 package com.github.salimkayabasi.glamdring.utils;
 
-import android.content.Context;
-
 import timber.log.Timber;
 
 public final class LogUtil {
@@ -9,10 +7,9 @@ public final class LogUtil {
   private LogUtil() {
   }
 
-  public static void init(Context context) {
+  public static void init() {
     Timber.plant(new Timber.DebugTree());
     Timber.plant(new StethoUtil.StethoTree());
-    StethoUtil.init(context);
   }
 
 }
